3c57770470e561bf443f7677c677b2251ad3d012
freeefly <freefly3557@gmail.com>
2021-03-23 02:44:16 +0900

    feat : update controller Closes #3

--- src/main/java/com/freeefly/msscbrewery/web/controller/BeerController.java
+++ src/main/java/com/freeefly/msscbrewery/web/controller/BeerController.java
@@ -3,7 +3,6 @@ package com.freeefly.msscbrewery.web.controller;
 import com.freeefly.msscbrewery.services.BeerService;
 import com.freeefly.msscbrewery.web.model.BeerDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,27 +17,19 @@ public class BeerController {
 
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeer(@PathVariable UUID beerId) {
-        return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
     }
 
     // POST - create new beer
     @PostMapping
-    public ResponseEntity handlePost(@RequestBody BeerDto beerDto) {
-        BeerDto savedDto = beerService.saveNewBeer(beerDto);
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.LOCATION, "http://localhost:8080/api/v1/beer/" + savedDto.getId().toString());
-        return new ResponseEntity(headers, HttpStatus.CREATED);
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity handleUpdate(@PathVariable UUID beerId, @RequestBody BeerDto beerDto) {
-        beerService.updateBeer(beerId, beerDto);
+    public ResponseEntity updateBeerById(@PathVariable UUID beerId, @RequestBody BeerDto beerDto) {
         return new ResponseEntity(HttpStatus.NO_CONTENT); // 요청이 성공하였고, 바디는 비었음
     }
-
-    @DeleteMapping("/{beerId}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteBeer(@PathVariable UUID beerId) {
-        beerService.deleteBeer(beerId);
-    }
 }
+
+
